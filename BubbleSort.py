fa92c0bf5356e9687bc220fa1584070c53105661
ap <59622720+arspaper@users.noreply.github.com>
2024-04-10 10:45:24 +0300

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -30,3 +30,12 @@ def sortBBL(arr):
             break
 
     return arr
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
