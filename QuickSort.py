c5a98c6f9a91b6542ca4675205f190a884698dda
ap <59622720+arspaper@users.noreply.github.com>
2024-04-10 11:11:08 +0300

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -34,3 +34,12 @@ def sortQCK(arr, low, high):
         pi = partition(arr, low, high)
         sortQCK(arr, low, pi - 1)
         sortQCK(arr, pi + 1, high)
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
